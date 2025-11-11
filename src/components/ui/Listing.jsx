7deba731a2eb2848c6adf5292bdff77691b42e7f
Shaban Hashim <shabantararr381@gmail.com>
2025-11-11 22:38:34 +0500

    console removing

--- src/components/ui/Listing.jsx
+++ src/components/ui/Listing.jsx
@@ -36,7 +36,6 @@ const Listing = () => {
   const [selectedCountries, setSelectedCountries] = useState([]);
   const [selectedStatus, setSelectedStatus] = useState("all");
   const [syncing, setSyncing] = useState(false);
-  console.log(listing, "listingdata");
   const navigate = useNavigate();
 
   const [showAsinModal, setShowAsinModal] = useState(false);
@@ -582,8 +581,6 @@ const Listing = () => {
 
                 const codes = payload.map((p) => p.code);
                 // if you need ISO country codes for something else:
-                console.log("Selected market IDs:", values);
-                console.log("Selected country codes:", codes);
               }}
               options={CountryOptions}
               placeholder="Select countries"
